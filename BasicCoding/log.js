0ea51b2246c416009b482a2fa37a237bddf9f402
sunilrathod098 <sunilrajputhrathod@gmail.com>
2024-12-10 22:53:16 +0530

    Basics Code push in gitHub

---
+++ BasicCoding/log.js
@@ -0,0 +1,15 @@
+// console.log('sunil') // sunil
+// console.log(typeof null === "object") // true
+// //eval
+// // console.log(eval("2 + 2")); // 4
+// console.log(eval(typeof(("2 + 2"))));
+// console.log(eval(new String(("2 + 2"))));
+// console.log("1" + 2 + 3); // 123
+// console.log(1 + 2 + "3"); //33
+// console.log(1 + 5 + "2" + 3); //623
+
+
+
+
+
+
